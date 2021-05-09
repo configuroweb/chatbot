362a57cf4d8ac5fa70d8d7fbcb91106ac7d83766
Mauricio Sevilla Britto <msevillab@gmail.com>
2021-05-09 14:05:13 -0500

    Ultima Versión Funcional

--- initialize.php
+++ initialize.php
@@ -1,5 +1,5 @@
 <?php
-$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'configuroweb','password'=>'4b67deeb9aba04a5b54632ad19934f26','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://localhost/chatbot/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
